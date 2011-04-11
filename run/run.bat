7de36aea059515277a0ad94b4394ffe7a41772d6
eyalhoc <eyalhoc@3537bff0-a784-4790-94a2-221882f779a6>
2011-04-11 15:47:10 +0000

    added dos batch file for windows

---
+++ run/run.bat
@@ -0,0 +1,8 @@
+
+echo off
+
+..\..\..\robust.exe ../src/regfile.v -od out -header
+..\..\..\robust.exe ../src/regfile.h -od out -header
+..\..\..\robust.exe ../src/regfile.html -od out
+
+echo Completed RobustVerilog regfile run - results in run/out/
--- run/run.sh
+++ run/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo Starting RobustVerilog regfile run
-rm -rf out
-mkdir out
-
 ../../../robust ../src/regfile.v -od out -header
 ../../../robust ../src/regfile.h -od out -header
 ../../../robust ../src/regfile.html -od out
