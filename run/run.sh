e345585f9ff53cc5b3d1b9dc4d2c1adaffb64847
eyalhoc <eyalhoc@3537bff0-a784-4790-94a2-221882f779a6>
2011-05-25 16:36:53 +0000

    GUI support

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-../../../robust ../src/base/regfile.v -od out -list list.txt -listpath -header -gui ${@}
-../../../robust ../src/base/regfile.h -od out -header -gui ${@}
-../../../robust ../src/base/regfile.html -od out -gui ${@}
+../../../robust -ver
+if [ $? -eq 0 ];then
+  ROBUST=../../../robust
+else
+  echo "RobustVerilog warning: GUI version not supported - using non-GUI version robust-lite"
+  ROBUST=../../../robust-lite
+fi
+
+$ROBUST ../src/base/regfile.v -od out -list list.txt -listpath -header -gui ${@}
+$ROBUST ../src/base/regfile.h -od out -header -gui ${@}
+$ROBUST ../src/base/regfile.html -od out -gui ${@}
